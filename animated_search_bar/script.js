4a9270eacbf07a8acca80e6618de921fdab5d3ae
ceewa30 <ceewa30@gmail.com>
2024-09-12 12:46:59 -0400

    Animated search bar

---
+++ animated_search_bar/script.js
@@ -0,0 +1,6 @@
+const searchbarE1 = document.querySelector(".search-bar-container")
+const magniferE1 = document.querySelector(".magnifier")
+
+searchbarE1.addEventListener("click", () => {
+    searchbarE1.classList.toggle("active")
+});
